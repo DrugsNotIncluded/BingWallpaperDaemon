ab732643b1c343e1c3f5f65120bbf1baa65f5975
humanised_doll <fenteron@gmail.com>
2022-07-13 21:49:48 +0500

    Now it actually works

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "cpr/session.h"
 #include <bits/types/time_t.h>
 #include <cstdlib>
+#include <ostream>
 #include <stdio.h>
 #include <unistd.h>
 #include <nlohmann/json.hpp>
@@ -29,7 +30,8 @@ const std::vector<std::string> markets {"en-US", "zh-CN", "ja-JP", "en-AU", "en-
 const std::string base_url {"http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt="};
 fs::path user_home (std::getenv("HOME"));
 fs::path config_dir (".config");
-fs::path default_config_path = user_home / config_dir;
+fs::path config_name("bdw.cfg");
+fs::path default_config_path = user_home / config_dir / config_name;
 nlohmann::json current_image_info;
 
 nlohmann::json get_daily_wallpaper_info();
@@ -49,29 +51,30 @@ nlohmann::json get_daily_wallpaper_info() {
 }
 
 std::string get_wallpaper_link_from_info() {
-	auto base_image_url = current_image_info["urlbase"];
-	return base_image_url.dump() + "_" + configdata.resolution;
+	auto base_image_url = current_image_info["images"][0]["urlbase"];
+	return "https://www.bing.com" + base_image_url.get<std::string>() + "_" + configdata.resolution + ".jpg";
 }
 
 void read_parse_config(std::string config_path) {
-	std::ifstream config_file;
+	std::ifstream config_file(config_path);
 	std::string line;
 	while (std::getline(config_file, line)) {
-		std::istringstream iss(line);
 		std::string keyword, value;
-		if (!(iss >> keyword >> value)) {break;}
+		auto pos = line.find(' ', 0);
+		keyword = line.substr(0, pos);
+		value = line.substr(pos+1);
 		
-		if (keyword == "command") {configdata.command = keyword;}
-		if (keyword == "interval") {configdata.interval = keyword;}
-		if (keyword == "region") {configdata.region = keyword;}
-		if (keyword == "resolution") {configdata.resolution = keyword;}
-		if (keyword == "path") {configdata.path = keyword;}
+		if (keyword == "command") {configdata.command = value;}
+		if (keyword == "interval") {configdata.interval = value;}
+		if (keyword == "region") {configdata.region = value;}
+		if (keyword == "resolution") {configdata.resolution = value;}
+		if (keyword == "path") {configdata.path = value;}
 	}
 }
 
 bool check_new_image_awailable() {
 	auto info = get_daily_wallpaper_info();
-	if (info["hsh"] != current_image_info["hsh"]) {
+	if (info["images"][0]["hsh"] != current_image_info["images"][0]["hsh"]) {
 		current_image_info = info;
 		return true;
 	}
@@ -88,7 +91,7 @@ bool download_file(std::string download_url) {
 	else {return false;}
 }
 
-std::string exec(char* cmd) {
+std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
     std::string result;
 
@@ -111,8 +114,6 @@ std::string exec(char* cmd) {
     return result;
 }
 
-
-
 time_t date_to_epoch(int year, int month, int day) {
 	struct tm t = {0};
 	t.tm_year = year - 1900;
@@ -126,22 +127,20 @@ void sleep_until(time_t time_until) {
 }
 
 void sleep_until_enddate() {
-	std::string enddate = current_image_info["enddate"].dump();
-	int year = stoi(enddate.substr(0,4));
-	int month = stoi(enddate.substr(5,6));
-	int day = stoi(enddate.substr(7,8));
+	std::string enddate = current_image_info["images"][0]["enddate"].dump();
+	int year = stoi(enddate.substr(1,5));
+	int month = stoi(enddate.substr(6,7));
+	int day = stoi(enddate.substr(8,9));
 	auto enddate_time = date_to_epoch(year, month, day);
 	sleep_until(enddate_time);
 }
 
 void daemon() {
-	// before running daemon "current_image_info" must be init for the first time after startup
-	/* get current !!date!! -> if current date is less then image expire data sleep for that time, else check if image hash changed once in configdata.interval, and if it has changed
-	   download new image and run wallpaper-manager command, repeat from beginning */
-
 	while (1) {
 		// sleep until next day
+		std::cout << "[Waiting for next day]" << std::endl;
 		sleep_until_enddate();
+		std::cout << "[Checking for image post]" << std::endl;
 		// periodically check if new image is awailable, sleep for interval if not
 		while (!check_new_image_awailable()) {usleep(stoi(configdata.interval));}
 		std::string image_url = get_wallpaper_link_from_info();
@@ -151,8 +150,18 @@ void daemon() {
 }
 
 void init() {
+	std::cout << "[Parsing config]" << std::endl;
 	read_parse_config();
+	// std::cout << "Config:" << std::endl
+	// 		  << "wallpaper command: " << configdata.command << std::endl
+	// 		  << "region: " << configdata.region << std::endl
+	// 		  << "screen resolution: " <<  configdata.resolution << std::endl
+	// 		  << "update interval: " << configdata.interval << std::endl
+	// 		  << "image download path" << configdata.path << std::endl;
 	current_image_info = get_daily_wallpaper_info();
+	std::string image_url = get_wallpaper_link_from_info();
+	download_file(image_url);
+	exec(configdata.command.c_str());
 }
 
 int main(int argc, char *argv[]) {
